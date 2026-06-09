b4c304072d19cb62febf39d29c2799a58d190b06
John <roblox888i12345@gmail.com>
2026-06-09 19:49:56 -0400

    Fix syntax error in news.js try block

--- commands/news.js
+++ commands/news.js
@@ -3,21 +3,21 @@ const components = require('../components/export');
 
 async function news(interaction) {
     await interaction.deferReply();
-    try {
-        const information = await get('https://api.scratch.mit.edu/news?limit=3');
-    
-        if (information && response.length > 0) {
-            const newsList = response.map(function(item) {
-                return {
-                    headline: item.headline,
-                    copy: item.copy
-                };
-            });
-            return interaction.editReply(components.news(newsList));
-        } else {
-            throw new Error("500: News might be down from the API");
-        }    
-    } catch (error) {
+try {
+    const information = await get('https://api.scratch.mit.edu/news?limit=3');
+
+    if (information && response.length > 0) {
+        const newsList = response.map(function(item) {
+            return {
+                headline: item.headline,
+                copy: item.copy
+            };
+        });
+        return interaction.editReply(components.news(newsList));
+    } else {
+        throw new Error("500: News might be down from the API");
+    }    
+} catch (error) {
         return interaction.editReply(components.container(
             "Error while fetching news information",
             16756224
